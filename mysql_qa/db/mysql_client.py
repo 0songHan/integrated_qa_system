85025ba6d88a66c088db1b32c50ae1be78cd07e2
chaosong <345279710@qq.com>
2026-02-05 10:27:52 +0800

    2026-2-5-second

---
+++ mysql_qa/db/mysql_client.py
@@ -0,0 +1,24 @@
+
+import pymysql
+import pandas as pd
+from base.config import Config
+from base.logger import logger
+
+
+class MySQLClient:
+    def __init__(self):
+        self.logger = logger
+        try:
+            self.connection = pymysql.connect(
+                host=Config().MYSQL_HOST,
+                user=Config().MYSQL_USER,
+                database=Config().MYSQL_DATABASE
+            )
+            self.cursor = self.connect.cursor()
+            self.logger.info('MySQL 连接成功')
+        except pymysql.MySQLErrot as e:
+            self.logger.error(f'MySQL 连接失败：{e}')
+            raise
+
+if __name__ == '__main__':
+    mysql_client = MySQLClient()
---
+++ mysql_qa/db/run.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/root/workspace/integrated_qa_system
+python3 mysql_client.py
